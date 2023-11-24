a6164a2dfe47fd6d1c29abf9e6046d07ba8f9e5a
EasyTools001 <150420149+EasyTools001@users.noreply.github.com>
2023-11-24 15:22:35 +0800

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,320 @@
+
+let title ; 
+let loadingFinished = false ;
+let time ;
+let howMuchChoosed = 0 ;
+let string2 ;
+let price = 150 ;
+
+let innerSave ;
+
+
+
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "V_"
+
+    }, 300);
+
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "Vi_"
+
+    }, 600);
+
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "Vib_"
+
+    }, 900);
+
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "Vibe_"
+
+    }, 1300);
+
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "VibeC_"
+
+    }, 1900);
+
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "VibeCi_"
+
+    }, 2300);
+    
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "VibeCin_"
+
+    }, 2600);
+
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "VibeCine_"
+
+    }, 3000);
+   setTimeout(() => {
+    
+        
+    title = document.getElementById("title")
+    title.innerHTML = "VibeCine"
+
+
+   }, 3500);
+
+    
+
+
+    console.warn("The loading has ended it took : " + "300ms" )
+    console.error("Do not let anyone write something here !")
+
+
+function season1DMSchoosed() {
+    
+    price = price + 350 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML =   "Demon Slayer S1 - "
+    
+
+}
+function season2DMSchoosed() {
+    
+    price = price + 350 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + "Demon Slayer S2 -"
+
+}
+function season1TBchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " The Boys Season 1-"
+
+}
+function season2TBchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " The Boys Season 2-"
+
+}
+function season3TBchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " The Boys Season 3-"
+
+}
+function season1Ychoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " You season 1 -"
+
+}
+function season2Ychoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " You season 2 -"
+
+}
+function season1SHchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Shameless season 1 -"
+
+}
+function season2SHchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Shameless season 2 -"
+
+}
+function season3SHchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Shameless season 3 -"
+
+}
+function season4SHchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Shameless season 4 -"
+
+}
+function season5SHchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Shameless season 5 -"
+
+}
+function season6SHchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Shameless season 6 -"
+
+}
+function season7SHchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Shameless season 7 -"
+
+}
+function season8SHchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Shameless season 8 -"
+
+}
+function season1PBchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Peaky Blinders 1 -"
+
+
+}
+
+function season2PBchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Peaky Blinders 2 -"
+
+
+}
+
+function season3PBchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Peaky Blinders 3 -"
+
+
+}
+
+function season4PBchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Peaky Blinders 4 -"
+
+
+}
+
+function season5PBchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Peaky Blinders 5 -"
+
+
+}
+
+
+function season6PBchoosed() {
+
+    price = price + 450 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Peaky Blinders 6 -"
+
+
+}
+
+function GHSallChoosed() {
+
+    price = price + 350 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " God of High School All episodes  -"
+
+}
+
+function MSAllChoosed() {
+
+    price = price + 350 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Mashle All episodes  -"
+
+
+}
+
+function KAchoosedOne() {
+
+    price = price + 350 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Kenga Ashura Season 1  -"
+
+
+}
+
+
+function KA2choosedOne() {
+
+    price = price + 350 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Kenga Ashura Season 2  -"
+
+
+}
+function HPchoosedall() {
+
+    price = price + 350 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " Hell's Paradise All Episodes  -"
+
+
+}
+f
+function MHSchoosed6(){
+
+    price = price + 350 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + " My Hero Academia All Episodes -"
+
+
+}
+function CHchoosedall(){
+
+    price = price + 350 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + "Chainsaw man All Episodes -"
+
+
+}
+
+function DSchoosed1() {
+
+    price = price + 350 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + "Dr Stone Season 1-"
+
+}
+
+function DSchoosed2() {
+
+    price = price + 350 ;
+    document.getElementById("price").innerHTML = price ;
+    string2 = document.getElementById("firstSegment").innerHTML = string2 + "Dr Stone Season 2-"
+
+}
